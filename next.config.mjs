a5bd08ed35f8401112e5b9eb3de59063f94a550a
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-12-16 13:19:34 -0800

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ucarecdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wordpress-1269066-4577871.cloudwaysapps.com',
+      },
+    ],
+  },
+}
+
+export default withNextIntl(nextConfig);
